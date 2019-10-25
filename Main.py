d61246ae475a63905b8e5d256b0bdab658a4786c
Saksham Sharma <53181089+sakshams02@users.noreply.github.com>
2019-10-25 16:33:38 +0530

    Update Main.py
    'import opencv' starts the other file which is the main code

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ def exitcoded():
     parent.destroy()
 def executecoded():
     parent.destroy()
-    import test2   
+    import opencv   
 qbutton = Button(parent, text = "I don't want to Draw!",command=exitcoded,bg = "orange",fg="white",activeforeground="white",activebackground="red")
 qbutton.pack( side ="bottom",fill="x")
 stbutton = Button(parent, text = "Let's go and test out this super innovative application!",command=executecoded, bg = "orange",fg="white",activeforeground="white",activebackground="green",highlightcolor="yellow")  
